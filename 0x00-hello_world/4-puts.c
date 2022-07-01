437d02e907a139630023e58c4404798588c26edc
Koulaange <koulaange2@gmail.com>
2022-07-01 06:58:47 -0700

    C Programm function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
-/
+#include <string.h> 
 
- * main - Entry point
- * Return: Always 0 (sucess)  
- */
-
- int main(void)
-
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
-
 }
-
-
