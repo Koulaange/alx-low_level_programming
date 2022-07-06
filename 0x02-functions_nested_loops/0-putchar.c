4fc20fd11737239542028734c1bde04e7ca2e0a9
Koulaange <koulaange2@gmail.com>
2022-07-05 18:26:19 -0700

    a program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,28 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
-* main -  prints Holberton, followed by a new line
+* main -  prints followed by a new line
 *
 * Return: 0 (Success)
 */
 int main(void)
 {
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+void num(int r, int n);
+
 _putchar('_');
 _putchar('p');
 _putchar('u');
