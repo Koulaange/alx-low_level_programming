81d653edd8367705844f27082e83ebc34c9939ee
Koulaange <koulaange2@gmail.com>
2022-07-05 18:31:04 -0700

    a program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -34,6 +34,4 @@ _putchar('r');
 _putchar('\n');
 return (0);
 
-}                                                                                             
-
-
+}                                                                    
