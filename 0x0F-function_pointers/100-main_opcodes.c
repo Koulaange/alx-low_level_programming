9e600863fc15fae60690cc0444ef9c517bdca5d2
Koulaange <107351905+Koulaange@users.noreply.github.com>
2022-08-04 02:36:54 +0000

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,40 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <udis86.h>
-
 /**
-  * main - ...
-  * @argc: ...
-  * @argv: ...
-  *
-  * Return: ...
-  */
+ * main - prints the opcodes of its own main function.
+ * @argc: arguments counter
+ * @argv: arguments values
+ *
+ * Return: result
+ */
 int main(int argc, char *argv[])
 {
-	ud_t ud_obj;
-	int val = 0, i = 0;
+	int n;
+	char *ptr = (char *)main;
+	int a;
 
-	if (argc == 2)
+	if (argc != 2)
 	{
-		val = atoi(argv[1]);
-
-		if (val < 0)
-		{
-			printf("Error\n");
-			exit(2);
-		}
-
-		ud_unit(&ud_obj);
-		ud_set_input_buffer(&ud_obj, argv[1], val);
-		ud_set_mode(&ud_obj, 64);
-		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
-
-		while (ud_disassemble(&ud_obj))
-		{
-			printf("\t%s\n", ud_insn_hex(&ud_obj));
-		}
+		printf("Error\n");
+		exit(1);
 	}
 
-	return (0);
-}
+	n = atoi(argv[1]);
 
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (a = 0; a < n - 1; a++)
+	{
+		printf("%02hhx ", ptr[a]);
+	}
+
+	printf("%02hhx\n", ptr[a]);
+	return (0);
+
+
+}
